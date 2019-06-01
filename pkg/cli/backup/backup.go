4f5dd865a60273c614bdb2c0dc03c2296725b6e8
Myroslav Gavryliak <efristical@gmail.com>
2019-06-01 16:24:51 +0300

    getting list of all repos at once

--- pkg/cli/backup/backup.go
+++ pkg/cli/backup/backup.go
@@ -65,44 +65,13 @@ func NewCommand() *cobra.Command {
 		Use:   "backup",
 		Short: "Backup command",
 		Run: func(cmd *cobra.Command, args []string) {
-			var repositories RepositoryWrapper
 			username := cmd.Flag("username").Value.String()
 			password := cmd.Flag("password").Value.String()
-			client := http.DefaultClient
-
-			for _, teamName := range teams(Creds{username, password}) {
-				for page := 1; ; page++ {
-					fmt.Printf("[ %s ] Doing %v page\n", teamName, page)
-
-					url := fmt.Sprintf(BitbucketRepositoriesAPI, teamName, page)
-					fmt.Printf("Sending request to: %s\n", url)
-
-					request, err := http.NewRequest("GET", url, nil)
-					if err != nil {
-						log.Fatalf("Failed sending GET request to Bitbucket: %s", err)
-					}
-
-					request.Header.Add("Content-Type", "application/json")
-					request.SetBasicAuth(username, password)
-
-					response, err := client.Do(request)
-					if err != nil {
-						log.Fatalf("Failed making request: %s", err)
-						return
-					}
-					defer response.Body.Close()
-
-					err = json.NewDecoder(response.Body).Decode(&repositories)
-
-					time.Sleep(2 * time.Second)
-
-					if len(repositories.Repositories) == 0 {
-						break
-					}
-				}
-			}
 
 			fmt.Println("Finished pulling list of repositories")
+
+			repos := repos(Creds{username, password})
+			fmt.Println(repos)
 
 			// backup.Clone()
 		},
@@ -150,4 +119,51 @@ func teams(creds Creds) []string {
 	}
 
 	return teamNames
+}
+
+// repos returns list of repositories of current bitbucket user
+func repos(creds Creds) []string {
+	var repositories RepositoryWrapper
+	var respositoryNames []string
+	client := &http.Client{}
+
+	for _, teamName := range teams(creds) {
+		for page := 1; ; page++ {
+			fmt.Printf("[ %s ] Doing %v page\n", teamName, page)
+
+			url := fmt.Sprintf(BitbucketRepositoriesAPI, teamName, page)
+			fmt.Printf("Sending request to: %s\n", url)
+
+			request, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				log.Fatalf("Failed sending GET request to Bitbucket: %s", err)
+			}
+
+			request.Header.Add("Content-Type", "application/json")
+			request.SetBasicAuth(creds.Username, creds.Password)
+
+			response, err := client.Do(request)
+			if err != nil {
+				log.Fatalf("Failed making request: %s", err)
+				return []string{}
+			}
+			defer response.Body.Close()
+
+			err = json.NewDecoder(response.Body).Decode(&repositories)
+
+			fmt.Printf("Repositories: %+v", repositories)
+			for _, repo := range repositories.Repositories {
+				// FullName is a teamname or username + repo name
+				respositoryNames = append(respositoryNames, repo.FullName)
+			}
+
+			time.Sleep(2 * time.Second)
+
+			if len(repositories.Repositories) == 0 {
+				break
+			}
+		}
+	}
+
+	return respositoryNames
 }
